5c1913732d99953a6ae074e6ede590ca6404d9f2
hnnhcmmngs <hannahcummings94@gmail.com>
2024-02-10 15:54:22 -0500

    Add functionality to change grid size
    Before, the grid size could not be changed. Now, there is a
    button at the top of the webpage that allows the user to change
    the grid size. When it is clicked, a popup asks the user to enter
    a number of squares per side for the new grid. If a valid number
    is entered, the current squares in the grid are removed and the
    grid is rebuilt. The limit for the size of the grid side is 100
    squares to prevent the webpage from becoming too slow.

--- js/script.js
+++ js/script.js
@@ -1,20 +1,41 @@
 const body = document.querySelector("body");
 const gridOfSquares = document.querySelector("#gridofsquares");
+const gridSize = document.querySelector("#gridsize");
 
-for (let i = 0; i < 16; i ++) {
-    let outerDiv = document.createElement("div");
-    outerDiv.style.display = "flex";
-    outerDiv.style.margin = "0px";
-    outerDiv.style.padding = "0px";
-    for (let j = 0; j < 16; j++) {
-        let innerDiv = document.createElement("div");
-        innerDiv.style.width = "45px";
-        innerDiv.style.height = "45px";
-        innerDiv.style.margin = "0px";
-        innerDiv.addEventListener("mouseenter", () => {
-            innerDiv.style.backgroundColor = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
-        })
-        outerDiv.appendChild(innerDiv);
+let side = 16;
+
+gridSize.addEventListener("click", () => {
+    let size = prompt("Enter a grid dimension:", side);
+    if (!Number.isNaN(size) && +size >= 1 && +size <= 100) {
+        side = +size;
+        createGrid(+size);
     }
-    gridOfSquares.appendChild(outerDiv);
+})
+
+function createGrid(gridSide) {
+    while (gridOfSquares.firstChild) {
+        gridOfSquares.removeChild(gridOfSquares.lastChild);
+    }
+    for (let i = 0; i < gridSide; i ++) {
+        let outerDiv = document.createElement("div");
+        outerDiv.style.display = "flex";
+        outerDiv.style.margin = "0px";
+        outerDiv.style.padding = "0px";
+        outerDiv.style.justifyContent = "space-between";
+        outerDiv.style.alignItems = "stretch";
+        outerDiv.style.flex = "auto";
+        outerDiv.style.gap = "0px";
+        for (let j = 0; j < gridSide; j++) {
+            let innerDiv = document.createElement("div");
+            innerDiv.style.flex = "auto";
+            innerDiv.style.margin = "0px";
+            innerDiv.addEventListener("mouseenter", () => {
+                innerDiv.style.backgroundColor = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
+            })
+            outerDiv.appendChild(innerDiv);
+        }
+        gridOfSquares.appendChild(outerDiv);
+    }
 }
+
+createGrid(side);
