32e41eb50383e2dd6437c1765b9b9b59ea53c70e
hnnhcmmngs <hannahcummings94@gmail.com>
2024-02-09 20:47:19 -0500

    Create 16 by 16 grid of squares
    The etch-a-sketch is represented by a 16 by 16 grid of squares.
    These squares have been created dynamically in the JavaScript
    file rather than the html file. The grid is created with nested
    flexboxes. For now, each grid square is given a random color as
    a background in order to visualize the structure of the grid.

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const body = document.querySelector("body");
+const gridOfSquares = document.querySelector("#gridofsquares");
+
+for (let i = 0; i < 16; i ++) {
+    let outerDiv = document.createElement("div");
+    outerDiv.style.display = "flex";
+    outerDiv.style.margin = "0px";
+    outerDiv.style.padding = "0px";
+    for (let j = 0; j < 16; j++) {
+        let innerDiv = document.createElement("div");
+        innerDiv.style.backgroundColor = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
+        innerDiv.style.width = "45px";
+        innerDiv.style.height = "45px";
+        innerDiv.style.margin = "0px";
+        outerDiv.appendChild(innerDiv);
+    }
+    gridOfSquares.appendChild(outerDiv);
+}
